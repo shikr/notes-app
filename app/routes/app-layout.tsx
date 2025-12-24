63b236ae0f3adb5a0df8a3af44b904f04f3ab425
shikr <cristobal.rod0907@gmail.com>
2025-12-24 14:10:02 -0600

    feat: add loader to app-layout

--- app/routes/app-layout.tsx
+++ app/routes/app-layout.tsx
@@ -1,15 +1,17 @@
+import { parseCookie } from 'cookie'
 import { Outlet } from 'react-router'
 import { Navbar } from '~/common/components/navbar'
+import { database } from '~/database/context'
 import type { Route } from './+types/app-layout'
+import { getUserFromAccessToken } from './auth/services/auth.server'
 
-export interface User {
-  id: string
-  name: string
-}
+export async function loader({ request }: Route.LoaderArgs) {
+  const cookie = request.headers.get('Cookie')
+  const { accessToken } = cookie !== null ? parseCookie(cookie) : {}
+  if (cookie === null || accessToken === undefined) return { user: null }
 
-export function loader(_: Route.LoaderArgs) {
-  // Placeholder for future user loading logic
-  const user: User | null = null
+  const db = database()
+  const user = await getUserFromAccessToken(db, accessToken)
 
   return { user }
 }
