ef160e9c66ff81e0adebd67496a83a4c0df7b945
shikr <cristobal.rod0907@gmail.com>
2025-12-23 20:44:54 -0600

    feat: add new note button to note collection header

--- app/routes/home/components/note-collection.tsx
+++ app/routes/home/components/note-collection.tsx
@@ -1,3 +1,6 @@
+import { Link, useRouteLoaderData } from 'react-router'
+import { Button } from '~/common/components/button'
+import type { User } from '~/routes/app-layout'
 import NoteItem, { type Note } from './note'
 
 export interface NoteInventoryProps {
@@ -5,10 +8,17 @@ export interface NoteInventoryProps {
 }
 
 export function NoteCollection({ notes }: NoteInventoryProps) {
+  const data = useRouteLoaderData<{ user: User | null }>('routes/app-layout')
+
   return (
     <section className="flex flex-col items-center justify-center pt-8 pb-4 px-16 gap-4">
-      <header className="self-start">
+      <header className="flex items-center justify-between w-full">
         <h1 className="text-2xl font-bold">Notes</h1>
+        {(data?.user ?? null) !== null && (
+          <Button as={Link} to="/note" radius="md" className="px-2">
+            New note
+          </Button>
+        )}
       </header>
       {notes.length === 0 ? (
         <p className="text-gray-500 text-xl">No notes available.</p>
